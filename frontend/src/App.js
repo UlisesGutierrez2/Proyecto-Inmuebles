286234e509ebd8d8e5f44dcd1eb769922affabdb
FacuReartes <facureartes9@gmail.com>
2023-06-17 19:31:50 -0300

    front modif

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Menu} from './components/Menu';
+import { Inicio } from './components/Inicio';
+
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Vencimientos from './components/Vencimientos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+          <Menu />
+          <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route path="/vencimientos" element={<Vencimientos />} />
+              <Route path="*" element={<Navigate to="/inicio" replace />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+
+    
     </div>
   );
 }
